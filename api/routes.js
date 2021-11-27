41c0c175d77510fab4eba25dccec338d5e4a7505
Harsh Vardhan <harsh.vardhan2019@vitstudent.ac.in>
2021-11-27 16:13:38 +0530

    dcdcvfdv

--- api/routes.js
+++ api/routes.js
@@ -5,6 +5,6 @@ const router = express.Router()
 //Users APIs
 router.route("/api/getentries").get(entriesctrl.getentries)
 router.route("/api/postentrie").post(entriesctrl.postentrie)
-router.route("/api/getpentries").post(entriesctrl.getentries)
+router.route("/api/getpentries").get(entriesctrl.getentries)
 
 export default router
